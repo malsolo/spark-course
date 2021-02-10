b1e666829a9581bc3e698d6a4014639901b3761d
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-02-10 09:29:28 +0100

    spark-course: word count with RDD.

---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("RDD Quickstart").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val fileName = "spark_readme.md"
+    val textFile = sc.textFile(fileName)
+    val wordCount = textFile.flatMap(line => line.split(" "))
+      .groupBy(identity).count()
+    println(s"word count?: $wordCount")
+    val count = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+      .collect()
+    println(s"words count: ${count.mkString("Array(", ", ", ")")}")
+  }
+
+}
